0526e4a0227654dccaa35ab97bd5d3046cde8be2
Chris <chrisjturner@gmail.com>
2020-05-02 22:39:35 +0100

    Add data view window

--- psychsim-gui-qt.py
+++ psychsim-gui-qt.py
@@ -1,9 +1,18 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets,   uic
+from data_view import dataView
 
 qtCreatorFile = "psychsim-gui-main.ui"
+data_view_file = "data-view.ui"
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+ui_dataView, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+
+class RawDataWindow(QtWidgets.QMainWindow, ui_dataView):
+    #TODO: is this way better? https://www.codementor.io/@deepaksingh04/design-simple-dialog-using-pyqt5-designer-tool-ajskrd09n
+    def __init__(self, parent=None):
+        super(RawDataWindow, self).__init__(parent)
 
 
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -19,13 +28,15 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.run_sim_button.setEnabled(False)
         self.run_sim_button.clicked.connect(self.run_simulation)
         self.actionSelect_simulation.triggered.connect(self.load_simulation)
+        self.select_sim.clicked.connect(self.load_simulation)
+        self.actionview_data.triggered.connect(self.show_data_window)
 
     def load_simulation(self):
         options = QtWidgets.QFileDialog.Options()
         # options |= QtWidgets.QFileDialog.DontUseNativeDialog
         fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","Python Files (*.py)", options=options)
         if fileName:
-            self.loaded_sim_label.setText(f"Loaded Sim: {str(fileName).split('/')[-1]}")
+            self.sim_path.setText(f"{str(fileName).split('/')[-1]}")
             self.run_sim_button.setEnabled(True)
             print(fileName)
 
@@ -40,9 +51,12 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             self.sim_running = True
             self.print_sim_output("SIM STOPPED")
 
-
     def print_sim_output(self, msg):
         self.simulation_output.setText(msg)
+
+    def show_data_window(self):
+        self.nd = RawDataWindow(self)
+        self.nd.show()
 
 
 if __name__ == "__main__":
