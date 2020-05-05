e2399076986f5659dec77ce3009101582169e8fb
Chris <chrisjturner@gmail.com>
2020-05-05 13:14:36 +0100

    Add base class for simulation

---
+++ SimBase.py
@@ -0,0 +1,18 @@
+"""
+Base class for simulations to be used with psychsim-gui-qt
+"""
+
+class SimBase:
+    def __init__(self):
+        self.sim_steps = 20
+
+    def run_sim(self):
+        """
+        overwrite this function
+        returns dictionary e.g. self.result0 = {'TriageAg1': {}}; return self.result0
+        """
+        pass
+
+if __name__ == "__main__":
+    sim = SimBase()
+    # sim.run_sim()
