783cc7cd13112087bdb26406c0b8ae59feb946f2
Chris <chrisjturner@gmail.com>
2020-05-27 10:00:33 +0100

    Add icon

--- PsychSimGui.py
+++ PsychSimGui.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from ui.PsychSimGuiMainWindow import PsychSimGuiMainWindow
 
 if __name__ == "__main__":
     sys.argv.append("--disable-web-security")
     app = QApplication(sys.argv)
     window = PsychSimGuiMainWindow()
+    window.setWindowIcon(QIcon('icon.png'))
     window.show()
     sys.exit(app.exec_())
