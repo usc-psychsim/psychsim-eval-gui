1053be7fb28d377139a4851c17180186a3e8987f
Chris Turner <Christopher.Turner@glasgow.ac.uk>
2020-11-05 11:06:40 +0000

    Fix stepping through dataframes

--- ui/StepThroughQueryWindow.py
+++ ui/StepThroughQueryWindow.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import sys
 import os
+import traceback
 import pandas as pd
 from ui.PandasModel import PandasModel
 
@@ -22,31 +23,63 @@ class StepThroughResultsWindow(QMainWindow, ui_window):
         self.selection_window = (0, 1)  # (starting time step, number of time steps to view)
 
         self.veiw_data_button.clicked.connect(self.view_data)
-        self.step_back_button.clicked.connect(self.step_forward)
-        self.step_forward_button.clicked.connect(self.step_backward)
+        self.step_forward_button.clicked.connect(lambda: self.step_through_data(type="forward"))
+        self.step_back_button.clicked.connect(lambda: self.step_through_data(type="backward"))
+        self.no_steps_spin.valueChanged.connect(self.set_no_steps)
 
         max_steps = self.query.results.shape[1]
         self.no_steps_label.setText(f"Select number of steps to view (max = {max_steps})")
+        self.no_steps_spin.setMinimum(1)
         self.no_steps_spin.setMaximum(max_steps)
 
+        self.no_steps = 1
+
+    def set_no_steps(self):
+        self.no_steps = self.no_steps_spin.value()
+        self.step_back_button.setText(f"< Step {self.no_steps} steps back")
+        self.step_forward_button.setText(f"Step {self.no_steps} steps forward >")
+
+
     def view_data(self):
-        # create a new dataframe with only the variables we want
         step_through_data = self.query.results
+        #todo: create a new dataframe with only the variables we want
+
         # only select the number of steps
-        self.selection_window = (0, self.no_steps_spin.value())
+        self.selection_window = (0, self.no_steps)
         step_through_data = step_through_data.iloc[:, self.selection_window[0]:self.selection_window[1]]
         # display the data
         model = PandasModel(data=step_through_data)
         self.results_table.setModel(model)
 
-    def step_forward(self):
-        # adjust the selection window
-        # create a new dataframe with the new selection window
-        # display dataframe
-        pass
+    def step_through_data(self, type):
+        try:
+            step_through_data = self.query.results
+            # create a new dataframe with the new selection window
+            if type == "forward":
+                step_start = self.selection_window[0] + self.no_steps
+                step_end = self.selection_window[1] + self.no_steps
+            if type == "backward":
+                step_start = self.selection_window[0] - self.no_steps
+                step_end = self.selection_window[1] - self.no_steps
 
-    def step_backward(self):
-        pass
+            # deal with viewing the extreme ends (and stopping)
+            if step_start < 0:
+                step_start = 0
+                step_end = 0 + self.no_steps
+            elif step_end > step_through_data.shape[1]:
+                step_start = step_through_data.shape[1] - self.no_steps
+                step_end = step_through_data.shape[1]
+
+            self.selection_window = (step_start, step_end)
+
+            step_through_data = step_through_data.iloc[:, step_start:step_end]
+            
+            # display dataframe
+            model = PandasModel(data=step_through_data)
+            self.results_table.setModel(model)
+        except:
+            tb = traceback.format_exc()
+            print(tb)
 
 
 
