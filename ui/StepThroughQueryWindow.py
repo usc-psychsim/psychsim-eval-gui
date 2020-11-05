1b932b0d352c4eba36cb8014f1dddb038391e0c5
Chris Turner <Christopher.Turner@glasgow.ac.uk>
2020-11-05 12:31:12 +0000

    Implement variable selection

--- ui/StepThroughQueryWindow.py
+++ ui/StepThroughQueryWindow.py
@@ -4,7 +4,9 @@ import sys
 import os
 import traceback
 import pandas as pd
+
 from ui.PandasModel import PandasModel
+from ui.CheckableComboBox import CheckableComboBox
 
 ui_file = os.path.join("ui", "step_through_query_window.ui")
 ui_window, QtBaseClass = uic.loadUiType(ui_file)
@@ -19,15 +21,26 @@ class StepThroughResultsWindow(QMainWindow, ui_window):
         self.setupUi(self)
         self.setWindowTitle("Step Through Results")
 
+        # set up the multi-select-combo
+        self.variable_combo_mult = CheckableComboBox()
+        vbox_layout = QVBoxLayout()
+        vbox_layout.addWidget(self.variable_combo_mult)
+        self.variable_select_widget.setLayout(vbox_layout)
+        self.variable_combo_mult.addItems(query.results.index.values)
+
         self.query = query
+        # self.display_data = self.query.results
+
         self.selection_window = (0, 1)  # (starting time step, number of time steps to view)
 
+        # connect the buttons
         self.veiw_data_button.clicked.connect(self.view_data)
         self.step_forward_button.clicked.connect(lambda: self.step_through_data(type="forward"))
         self.step_back_button.clicked.connect(lambda: self.step_through_data(type="backward"))
         self.no_steps_spin.valueChanged.connect(self.set_no_steps)
         self.no_steps_view_spin.valueChanged.connect(self.set_no_steps_view)
 
+        # set the steps to view and step through
         max_steps = self.query.results.shape[1]
         self.no_steps_view_label.setText(f"Select number of steps to view (max = {max_steps})")
         self.no_steps_view_spin.setMinimum(1)
@@ -46,21 +59,22 @@ class StepThroughResultsWindow(QMainWindow, ui_window):
         self.step_back_button.setText(f"< Step {self.no_steps} steps back")
         self.step_forward_button.setText(f"Step {self.no_steps} steps forward >")
 
-
     def view_data(self):
-        step_through_data = self.query.results
-        #todo: create a new dataframe with only the variables we want
+        cat_values = self.variable_combo_mult.currentData()
+        display_data = self.query.results.loc[cat_values]
 
         # only select the number of steps
         self.selection_window = (0, self.no_steps_view)
-        step_through_data = step_through_data.iloc[:, self.selection_window[0]:self.selection_window[1]]
+        display_data = display_data.iloc[:, self.selection_window[0]:self.selection_window[1]]
         # display the data
-        model = PandasModel(data=step_through_data)
+        model = PandasModel(data=display_data)
         self.results_table.setModel(model)
 
     def step_through_data(self, type):
+        # todo refactor this with the view_data function
+        cat_values = self.variable_combo_mult.currentData()
+        display_data = self.query.results.loc[cat_values]
         try:
-            step_through_data = self.query.results
             # create a new dataframe with the new selection window
             if type == "forward":
                 step_start = self.selection_window[0] + self.no_steps
@@ -73,16 +87,16 @@ class StepThroughResultsWindow(QMainWindow, ui_window):
             if step_start < 0:
                 step_start = 0
                 step_end = 0 + self.no_steps_view
-            elif step_end > step_through_data.shape[1]:
-                step_start = step_through_data.shape[1] - self.no_steps_view
-                step_end = step_through_data.shape[1]
+            elif step_end > display_data.shape[1]:
+                step_start = display_data.shape[1] - self.no_steps_view
+                step_end = display_data.shape[1]
 
             self.selection_window = (step_start, step_end)
 
-            step_through_data = step_through_data.iloc[:, step_start:step_end]
+            display_data = display_data.iloc[:, step_start:step_end]
 
             # display dataframe
-            model = PandasModel(data=step_through_data)
+            model = PandasModel(data=display_data)
             self.results_table.setModel(model)
         except:
             tb = traceback.format_exc()
