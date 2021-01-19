08888be005e84410603157562ece8626959b4381
Christopher Turner <christopherturner@Lucileca-mac.broadband>
2021-01-19 15:13:02 +0000

    Add debug object for agent decisions

--- sim_scripts/Benchmark.py
+++ sim_scripts/Benchmark.py
@@ -17,31 +17,34 @@ from model_learning.util.cmd_line import none_or_int
 from model_learning.util.plot import plot_bar
 from model_learning.util.io import get_files_with_extension, create_clear_dir, change_log_handler
 
+
+from appraisal import appraisal_dimentions as ad
+
 __author__ = 'Pedro Sequeira'
 __email__ = 'pedrodbs@gmail.com'
 __desc__ = 'Simple test script that loads several player log files and creates a plot with the corresponding ' \
            'imported trajectories\' length.'
 
 OUTPUT_DIR = 'output/benchmark'
-# DATA_FILE = None#'/Users/christopherturner/Documents/GLASGOW-MARSELLA/Data/ASU_2020_08/WithMissionTimer/processed_HSRData_TrialMessages_CondBtwn-NoTriageNoSignal_CondWin-FalconEasy-StaticMap_Trial-43_Team-na_Member-26_Vers-3.csv'#'/home/chris/Documents/GLASGOW_MARSELLA/processed_HSRData_TrialMessages_CondBtwn-NoTriageNoSignal_CondWin-FalconEasy-StaticMap_Trial-43_Team-na_Member-26_Vers-3.csv' #atomic/data/processed_ASIST_data_study_id_000001_condition_id_000002_trial_id_000010_messages.csv' #'data/glasgow/processed_20200724_Participant1_Cond1.csv'
-DATA_FILE = '/Users/christopherturner/Documents/GLASGOW-MARSELLA/Data/ASU_2020_08/WithMissionTimer/processed_HSRData_TrialMessages_CondBtwn-NoTriageNoSignal_CondWin-FalconEasy-StaticMap_Trial-43_Team-na_Member-26_Vers-3.csv'#'/home/chris/Documents/GLASGOW_MARSELLA/processed_HSRData_TrialMessages_CondBtwn-NoTriageNoSignal_CondWin-FalconEasy-StaticMap_Trial-43_Team-na_Member-26_Vers-3.csv' #atomic/data/processed_ASIST_data_study_id_000001_condition_id_000002_trial_id_000010_messages.csv' #'data/glasgow/processed_20200724_Participant1_Cond1.csv'
+DATA_FILE = None#'/Users/christopherturner/Documents/GLASGOW-MARSELLA/Data/ASU_2020_08/WithMissionTimer/processed_HSRData_TrialMessages_CondBtwn-NoTriageNoSignal_CondWin-FalconEasy-StaticMap_Trial-43_Team-na_Member-26_Vers-3.csv'#'/home/chris/Documents/GLASGOW_MARSELLA/processed_HSRData_TrialMessages_CondBtwn-NoTriageNoSignal_CondWin-FalconEasy-StaticMap_Trial-43_Team-na_Member-26_Vers-3.csv' #atomic/data/processed_ASIST_data_study_id_000001_condition_id_000002_trial_id_000010_messages.csv' #'data/glasgow/processed_20200724_Participant1_Cond1.csv'
+# DATA_FILE = '/Users/christopherturner/Documents/GLASGOW-MARSELLA/Data/ASU_2020_08/WithMissionTimer/processed_HSRData_TrialMessages_CondBtwn-NoTriageNoSignal_CondWin-FalconEasy-StaticMap_Trial-43_Team-na_Member-26_Vers-3.csv'#'/home/chris/Documents/GLASGOW_MARSELLA/processed_HSRData_TrialMessages_CondBtwn-NoTriageNoSignal_CondWin-FalconEasy-StaticMap_Trial-43_Team-na_Member-26_Vers-3.csv' #atomic/data/processed_ASIST_data_study_id_000001_condition_id_000002_trial_id_000010_messages.csv' #'data/glasgow/processed_20200724_Participant1_Cond1.csv'
 
 
 # params
 NUM_TRAJECTORIES = 1#0
-TRAJ_LENGTH = 150
+TRAJ_LENGTH = 25
 RATIONALITY = 1 / 0.1  # inverse temperature
 SELECTION = 'distribution'
 PRUNE_THRESHOLD = 5e-2  # 1e-2 #'Likelihood below which stochastic outcomes are pruned.'
 KEEP_K = 10
 HORIZON = 2
-SEED = 0
+SEED = 1
 PROCESSES = None #'Number of processes/cores to use. If unspecified, all available cores will be used'
 
 LIGHTS = False
 NO_BEEP = 0.0
 
-MAP_NAME = 'FalconEasy' #'Name of the map for trajectory generation.'
+MAP_NAME = 'simple'#'FalconEasy' #'Name of the map for trajectory generation.'
 PLAYER_NAME = 'Player'
 FULL_OBS = True
 
@@ -159,9 +162,12 @@ if __name__ == '__main__':
         #               {'name': PREFER_YELLOW_MODEL, 'reward': {GREEN_VICTIM: LOW_VAL, YELLOW_VICTIM: HIGH_VAL},
         #                'rationality': MODEL_RATIONALITY, 'selection': MODEL_SELECTION}]
         #
-        #
-        # ADDING THIS SEEMS TO ADD MASSIVELY TO THE TIME (MAYBE JUST TRY ONE?)
+        # model_list = [{'name': PREFER_GREEN_MODEL, 'reward': {GREEN_VICTIM: HIGH_VAL, YELLOW_VICTIM: LOW_VAL},
+        #                'rationality': MODEL_RATIONALITY, 'selection': MODEL_SELECTION}]
         # set_player_models(world, observer.name, agent.name, victims, model_list)
+        # #
+        # model_list = agent.modelList.copy()
+        # # ADDING THIS SEEMS TO ADD MASSIVELY TO THE TIME (MAYBE JUST TRY ONE?)
 
         # generate trajectories
         logging.info('Generating {} trajectories of length {} using {} parallel processes...'.format(
@@ -171,37 +177,73 @@ if __name__ == '__main__':
         # player_name = agent.name
         # player = agent  # world.agents[player_name]
 
+        player_step_appraisal_info = {}
+
         step = 0
         trajectories = []
         # NOTE: use the while loop for generate_trajectory()
         #       otherwise you can just do generate_trajectories() and then loop over the results.
         while step <= TRAJ_LENGTH:
 
+            player_appraisal = ad.PlayerAppraisal()
+            player_pre_utility = {}
+
             if step == 0:
-                player_pre_utility = agent.getState("__REWARD__").domain()[0]
                 player_loc = player_loc_actual = world.getFeature(f"{agent.name}'s loc", agent.world.state)
+                for m in agent.models.keys():
+                    player_pre_utility[m] = agent.getState("__REWARD__").domain()[0]
             else:
-                player_pre_utility = traj_agent.getState("__REWARD__").domain()[0]
+                # player_pre_utility = traj_agent.getState("__REWARD__").domain()[0]
                 player_loc = player_loc_actual = traj_world.getFeature(f"{agent.name}'s loc", traj_world.state)
+
+                for m in traj_agent.models.keys():
+                    player_pre_utility[m] = traj_agent.getState("__REWARD__").domain()[0]
 
             logging.info("=================")
             logging.info(f"STEP: {step}")
             logging.info(f"PRE UTILITY: {player_pre_utility}")
             logging.info(f"PRE LOC: {player_loc}")
 
+
             # trajectories.append(generate_trajectories(
             #     agent, NUM_TRAJECTORIES, step, threshold=PRUNE_THRESHOLD, processes=PROCESSES, seed=SEED,
             #     verbose=True))
-            trajectories.append(generate_trajectory(agent, 1, threshold=PRUNE_THRESHOLD,
-                        seed=SEED, verbose=None))
+            debug = {'Player': {}}
+            # trajectories.append(generate_trajectory(agent, 1, threshold=PRUNE_THRESHOLD,
+            #             seed=SEED, verbose=None, debug=debug))
 
-            traj_world = trajectories[step][0][0]
+            step_info = {}
+            step_info["trajectory"] = generate_trajectory(agent, 1, threshold=PRUNE_THRESHOLD,
+                        seed=SEED, verbose=None, debug=debug)
+            step_info["debug"] = debug
+
+            trajectories.append(step_info.copy())
+
+            traj_world = step_info["trajectory"][0][0]
             traj_agent = traj_world.agents[agent.name]
-            logging.info(f"Action: {trajectories[step][0][1]}")
+            logging.info(f"Action: {step_info['trajectory'][0][1]}")
             logging.info(f"Action: {traj_world.getAction(agent.name, unique=True)}")
             logging.info(f"Action: {traj_agent.getState('__ACTION__').domain()[0]}")
             player_cur_utility = traj_agent.getState("__REWARD__").domain()[0]
-            logging.info(f"CURUTILITY: {player_cur_utility}")
+            logging.info(f"CUR UTILITY: {player_cur_utility}")
+
+            # for key, model_name in model_list.items(): #this is set earlier because each new step from generate_trajectory creates a bunch more models
+            # for model in model_list:  # this is set earlier because each new step from generate_trajectory creates a bunch more models
+            #     model_name = model['name']
+            #     pass
+            #     # player_cur_utility = player.getReward(m)# this is actually the reward function!!
+            #     player_appraisal = ad.PlayerAppraisal()
+            #     player_appraisal.motivational_relevance = ad.motivational_relevance(player_pre_utility[model_name],
+            #                                                                         player_cur_utility)
+            #     logging.info(f"MOTIVATIONAL REL: {player_appraisal.motivational_relevance}")
+            #     player_appraisal.motivational_congruence = ad.motivational_congruence(player_pre_utility[model_name],
+            #                                                                           player_cur_utility)
+            #     logging.info(f"MOTIVATIONAL CONG: {player_appraisal.motivational_congruence}")
+            #     # player_appraisal.novelty = #TODO: figure out what the possible actions are (legal?) and how to rank them
+            #     # player_appraisal.accountable = ad.accountability(...) # TODO: figure out who should be the observer (maybe this doesn't work in single player?)
+            #     # player_appraisal.control = #TODO: figure out how to do the projected action stuff
+            #     player_step_appraisal_info[step] = player_appraisal
+
             step = step + 1
         pass
 
