b793731477da20e9d4e45d40c19a5c75a3c2e926
Christopher Turner <christopherturner@Lucileca-mac.broadband>
2021-01-22 17:55:24 +0000

    Tidy code

--- sim_scripts/RunBenchmarkTrajectories.py
+++ sim_scripts/RunBenchmarkTrajectories.py
@@ -1,67 +1,35 @@
+"""
+This file is hacked together to get the output from the trajectories generated in benchmark.py (benchmark_copy.py) into the GUI
+"""
 
 import pickle
-
-file = open("output/benchmark/trajectories_pickle20210122-122352", 'rb')
-trajectories = pickle.load(file)
-file.close()
-
-agent_name = 'Player'
-
-
 from appraisal import appraisal_dimensions as ad
 
-player_step_appraisal_info = {}
-player_pre_utility = {}
 
-step = 0
-for t in trajectories[0]:
-    player_appraisal = ad.PlayerAppraisal()
+class RunBenchmarkTrajectories():
+    def __init__(self):
+        file = open("sim_scripts/output/benchmark/trajectories_pickle20210122-122352", 'rb')
+        # file = open("output/benchmark/trajectories_pickle20210122-122352", 'rb')
+        self.trajectories = pickle.load(file)
+        file.close()
+        self.agent_name = 'Player'
+        self.sim_steps = len(self.trajectories[0])
+        self.current_step = 0
 
-    traj_world = t[0]
-    traj_agent = traj_world.agents[agent_name]
-    traj_debug = t[2]
+    def run_step(self):
+        traj_world = self.trajectories[0][self.current_step][0]
+        traj_observer = traj_world.agents["ATOMIC"]
+        traj_debug = self.trajectories[0][self.current_step][2]
 
-    if step == 0:
-        # for key, model in traj_agent.modelList.items():
-        player_pre_utility = traj_agent.getState("__REWARD__").domain()[0] # THIS DOESN"T SEEM TO UPDATE
+        return_result = {"WORLD_STATE": traj_world.state,
+                         "AGENT_STATE": traj_debug,
+                         "TRAJECTORY": self.trajectories[0][self.current_step],
+                         "OBSERVER": traj_observer}
+        self.current_step = self.current_step + 1
+        return return_result
 
-    player_loc = player_loc_actual = traj_world.getFeature(f"{traj_agent.name}'s loc", traj_agent.world.state)
-
-    print("=================")
-    print(f"STEP: {step}")
-    print(f"PRE UTILITY: {player_pre_utility}")
-    print(f"PRE LOC: {player_loc}")
-
-    loc = traj_world.getFeature(f"{agent_name}'s loc", traj_world.state)
-    player_cur_utility = traj_agent.getState("__REWARD__").domain()[0]
-    print(f"LOC: {loc}")
-    legal_actions = traj_agent.getLegalActions()
-    print(f"LEGAL ACTIONS: {legal_actions}")
-    print(f"Action: {traj_agent.getState('__ACTION__').domain()[0]}")
-    print(f"CUR UTILITY: {player_cur_utility}")
-
-    # for model_name in ['Player0', 'prefer_green']:
-        #TODO: make this actually work over each model - though only different models (i.e. statically added models, not all the newly generated ones...
-
-    # player_cur_utility = player.getReward(m)# this is actually the reward function!!
-    player_appraisal = ad.PlayerAppraisal()
-    player_appraisal.motivational_relevance = ad.motivational_relevance(player_pre_utility,
-                                                                        player_cur_utility)
-    print(f"MOTIVATIONAL REL: {player_appraisal.motivational_relevance}")
-
-    player_appraisal.motivational_congruence = ad.motivational_congruence(player_pre_utility,
-                                                                          player_cur_utility)
-    print(f"MOTIVATIONAL CONG: {player_appraisal.motivational_congruence}")
-
-    # extract the possible actions and corresponding rewards from the trajectory
-    agent_decision = t[2][agent_name]["__decision__"]
-    ad.extract_expected_action_reward(agent_decision, agent_name)
-    # player_appraisal.novelty = #TODO: figure out what the possible actions are (legal?) and how to rank them
-    # player_appraisal.accountable = ad.accountability(...) # TODO: figure out who should be the observer (maybe this doesn't work in single player?)
-    # player_appraisal.control = #TODO: figure out how to do the projected action stuff
-    player_step_appraisal_info[step] = player_appraisal
-
-    player_pre_utility = player_cur_utility
-    step = step + 1
-
-pass
+if __name__ == "__main__":
+    sim = RunBenchmarkTrajectories()
+    for step in range(sim.sim_steps):
+        res = sim.run_step()
+    pass
