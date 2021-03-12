0ee71c4e985455b1faf6049c5bbf8db7bf60888e
Christopher Turner <christopherturner@Lucileca-mac.broadband>
2021-03-12 16:02:49 +0000

    Tidy code

--- appraisal/appraisal_dimensions.py
+++ appraisal/appraisal_dimensions.py
@@ -2,16 +2,14 @@
 Functions for appraisal dimensions
 """
 import math
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
+
 
 @dataclass
 class PlayerAppraisal:
     """
     Class to hold appraisal dimension info
     """
-    # TODO:
-    #  fix this to have only the relevant ones
-    #  figure out a better way to store this
     motivational_relevance: float = None
     motivational_congruence: float = None
     coerced: bool = False
@@ -22,26 +20,6 @@ class PlayerAppraisal:
     novelty: float = None
     consistency: float = None
     control: float = None
-
-
-    def desirability(self):
-        """
-        Something is desirable if it is relevant, and if it has a positive effect on the utility
-        """
-        if self.motivational_relevance and self.motivational_congruence > 0:
-            return True
-        else:
-            return False
-
-
-
-def calculate_utility(reward_fn, state):
-    """
-    This is a weighted sum of the reward for the current state
-    reward_fn = reward function
-    state = state
-    """
-    pass
 
 
 def extract_expected_action_reward(player_decision, player_name):
@@ -72,7 +50,8 @@ def motivational_relevance(pre_utility, cur_utility):
 
 def motivational_congruence(pre_utility, cur_utility):
     """
-    Motivational congruence or incongruence measures the extent to which the encounter thwarts or facilitates personal goals
+    Motivational congruence or incongruence measures the extent to which the encounter thwarts or facilitates
+    personal goals
 
     cur_utility: utility after event has happened
     pre_utility: utility before event has happened
@@ -86,6 +65,9 @@ def blame(team_pre_utility, team_cur_utility):
     Did the actor take an action that netatively impacted the team?
     i.e. did the action that the actor took
     return: scale of team utility difference
+
+    team_pre_utility: utility of team before action
+    team_cur_utility: utility of team after action
     """
     # If the actor did anything to negatively impact the team, they are to blame
     return team_cur_utility - team_pre_utility
@@ -95,6 +77,11 @@ def intended_blame(actor_pre_utility, actor_cur_utility, team_pre_utility, team_
     """
     Did the agent take an agent that negatively impacted the team, but positively impacted themselves?
     i.e. did the action that the actor took
+
+    actor_pre_utility: utility of actor before action
+    actor_cur_utility: utility of actor after action
+    team_pre_utility: utility of team before action
+    team_cur_utility: utility of team after action
     """
     # did the actor's action negatively affect the team?
     if team_cur_utility < team_pre_utility:
@@ -111,7 +98,11 @@ def intended_blame(actor_pre_utility, actor_cur_utility, team_pre_utility, team_
 def blame2(action, player_decision):
     """
     Did the actor take an action that wasn't the best (for the team - or for the actor who is asking)
-    TODO: fix this for multi-player (the player_decision should be the belief about another player's decision) - this assumes the '__EV__' here corresponds to the observing player's utility not the expected utility of the observed player
+    TODO: fix this
+        for multi-player (the player_decision should be the belief about another player's decision) - this assumes the
+        '__EV__' here corresponds to the observing player's utility not the expected utility of the observed player
+
+    player_decision: decision object of the player from psychsim
     """
     # -- FOR USE WITH 'random' SELECTION
     # actual_action = player_decision['action']
@@ -124,57 +115,8 @@ def blame2(action, player_decision):
     # -- FOR USE WITH 'distribution' SELECTION
     actual_action_utility = player_decision['V'][action]['__EV__']
     for k, v in player_decision['V'].items():
-        if v['__EV__'] > actual_action_utility: # the player could have taken a better option
+        if v['__EV__'] > actual_action_utility:  # the player could have taken a better option
             return True
-    return False
-
-def if_coerced(actor, pact, pre_utility):
-    """
-    Determines if the agent coerced to perform a specific action
-    actor: the agent being studied
-    pact: the action performed by actor
-    pre_utility: actor’s utility before doing pact (as observed by observing actor)
-    action_utility: the actor's utility after completing the action (as observed by observing actor) #TOOD: check this
-    """
-    action_utility = actor.getState("__REWARD__").domain()[0] # this is the current utility of the agent (afther the action was completed)
-    for action in actor.actionOptions(): #todo: fix this to be the correct code
-        if action != pact: #if there exists another action which does not hurt actor’s own utility
-            if action_utility >= pre_utility: #TODO: find out if utility should be part of a class? maybe we could inherit the actor class and add these functions so all this stuff is just available internaly?
-                return False
-    if action_utility < pre_utility:
-        return False
-    return True
-
-
-def is_coercer_for(agent, actor, agent_pact, actor_pact):
-    """
-    check if agent coerced actor
-    agent_pact: the action performed by agent
-    actor_pact: the action performed by actor
-    """
-    # for action in agent.actionOptions():
-    #     if action != agent_pact:
-    #         simulate action agent_pact #TODO: figure out this code
-    #         if not if_coerced(actor, actor_pact):
-    #             return True
-    # return False
-    pass
-
-
-def accountability(agent, actor, agent_pact, actor_pact, pre_utility):
-    """
-    Determines if an actor is accountable for a specific action
-    agent: agent who might be coerced
-    actor: the agent being studied
-    agent_pact: the action performed by agent
-    actor_pact: the action performed by actor
-    pre_utility: actor’s utility before doing pact (as observed by observing actor)
-    action_utility: the actor's utility after completing the action (as observed by observing actor) #TOOD: check this
-    """
-    if not if_coerced(actor, actor_pact, pre_utility):
-        return True  # This actor was responsible as there was no coercer
-    if not is_coercer_for(actor, agent, agent_pact, actor_pact):
-        return False  # the actor was at least partially responsible for coercing the agent
     return False
 
 
@@ -182,21 +124,23 @@ def control(player_decision):
     """
     This is a probability. For each predicted action that delivers a positive utility change, sum the probabilities.
     that is the control
+    player_decision: decision object of the player from psychsim
     """
-    control = 0
+    player_control = 0
     for action, predictions in player_decision['V'].items():
-        if predictions['__EV__'] > 0: # TODO: check this is ok (is it always if the expected reward is any positive value?)
-            control = control + player_decision['action'][action]
-    return control
-
+        if predictions['__EV__'] > 0:
+            player_control = player_control + player_decision['action'][action]
+    return player_control
 
 
 def novelty(num_possible_actions, action_rank):
     """
     how novel (unexpected) an action is compared to agent's beliefs
+    num_possible_actions: the number of possible actions that can be taken
+    action_rank: the rank of the action (actions are ranked from lowest to highest utility)
     """
     # TODO: fix this to take into account environment
-    return 1-consistency(num_possible_actions, action_rank)
+    return 1 - consistency(num_possible_actions, action_rank)
 
 
 def consistency(num_possible_actions, action_rank):
@@ -205,20 +149,9 @@ def consistency(num_possible_actions, action_rank):
     num_possible_actions: the number of possible actions that can be taken
     action_rank: the rank of the action (actions are ranked from lowest to highest utility)
     """
-    #TODO: Question - what if actions are equally ranked? Should they just count as one action?
+    # TODO: Question - what if actions are equally ranked? Should they just count as one action?
     denom = 0
     for i in range(num_possible_actions):
         denom = denom + math.exp(i)
     return math.exp(action_rank) / denom
 
-
-
-
-
-# EMA DIMENSIONS
-# Desirability. The value of a proposition to the agent (such as Does it ad- vance or inhibit its utility?) can be posi- tive or negative;
-# Likelihood. A measure of the likelihood of propositions;
-# Expectedness. The extent to which a state could have been predicted from the causal interpretation; = NOVELTY
-# Causal attribution. Who deserves credit/blame?;
-# Controllability. Can the outcome be altered through an agent’s actions?; and
-# Changeability. Can the outcome be altered by another agent?
