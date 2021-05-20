ab70b6a323dd2d8121abe7aa52375d96794e2a14
chrisjturner <chrisjturner@gmail.com>
2021-05-20 16:00:58 +0100

    Add more sim scripts based on updated PsyshSim

---
+++ sim_scripts/ForwardPlanningTom.py
@@ -0,0 +1,162 @@
+import logging
+import random
+from psychsim.agent import Agent
+from psychsim.probability import Distribution
+from psychsim.pwl import makeTree, equalRow, setToConstantMatrix, rewardKey
+from psychsim.world import World
+
+__author__ = 'Pedro Sequeira'
+__email__ = 'pedrodbs@gmail.com'
+__description__ = 'Example of using theory-of-mind in a game-theory scenario involving two agents in the iterated' \
+                  'version of the Prisoner\'s dilemma ' \
+                  '(https://en.wikipedia.org/wiki/Prisoner%27s_dilemma#The_iterated_prisoner%27s_dilemma)' \
+                  'Both agents follow a strategy inspired on tit-for-tat (https://en.wikipedia.org/wiki/Tit_for_tat)' \
+                  'Namely, the first action is open and depends on the beliefs of the agent about the other\'s ' \
+                  'behavior. From there on, retaliation is applied by always choosing defect after the first defection ' \
+                  'of the other agent (non-forgiving).' \
+                  'Hence the planning horizon has an influence on the agents\' decision to cooperate or defect:' \
+                  '- if horizon is 0, first action for each agent will be random (0 reward), then tit-fot-tat' \
+                  '- if horizon is 1, agents will always defect (one-shot decision, other\'s action does not matter' \
+                  '- if horizon is 2, first action for each agent will be random, because CC followed by CC == ' \
+                  'DC followed by DD = -2, so C or D have the same value independently of the other; then tit-fot-tat' \
+                  '- if horizon is >2, agents will always cooperate because they can see each other\'s tit-for-tat ' \
+                  'strategy using ToM, and hence believe the other will cooperate if they also cooperate, leading to ' \
+                  'highest mutual payoff in the long run.' \
+                  'Note: imperfect models can break this belief and make the agents cynical towards each other.'
+
+# parameters
+MAX_HORIZON = 4
+NUM_STEPS = 4
+TIEBREAK = 'random'  # when values of decisions are the same, choose randomly
+SEED = 0
+
+# decision labels
+NOT_DECIDED = 'none'
+DEFECTED = 'defected'
+COOPERATED = 'cooperated'
+
+# payoff parameters (according to PD)
+SUCKER = -3  # CD
+TEMPTATION = 0  # DC
+MUTUAL_COOP = -1  # CC
+PUNISHMENT = -2  # DD
+INVALID = -10000
+
+DEBUG = False
+
+
+# defines a payoff matrix tree (0 = didn't decide, 1 = Defected, 2 = Cooperated)
+def get_reward_tree(agent, my_dec, other_dec):
+    reward_key = rewardKey(agent.name)
+    return makeTree({'if': equalRow(my_dec, NOT_DECIDED),  # if I have not decided
+                     True: setToConstantMatrix(reward_key, INVALID),
+                     False: {'if': equalRow(other_dec, NOT_DECIDED),  # if other has not decided
+                             True: setToConstantMatrix(reward_key, INVALID),
+                             False: {'if': equalRow(my_dec, COOPERATED),  # if I cooperated
+                                     True: {'if': equalRow(other_dec, COOPERATED),  # if other cooperated
+                                            True: setToConstantMatrix(reward_key, MUTUAL_COOP),  # both cooperated
+                                            False: setToConstantMatrix(reward_key, SUCKER)},
+                                     False: {'if': equalRow(other_dec, COOPERATED),
+                                             # if I defected and other cooperated
+                                             True: setToConstantMatrix(reward_key, TEMPTATION),
+                                             False: setToConstantMatrix(reward_key, PUNISHMENT)}}}})
+
+
+class ForwardPlanningTom:
+    def __init__(self):
+        self.sim_steps = 50
+        random.seed(0)
+
+        # sets up log to screen
+        logging.basicConfig(format='%(message)s', level=logging.DEBUG if DEBUG else logging.INFO)
+
+        # create world and add agent
+        self.world = World()
+        self.agent1 = Agent('Agent 1')
+        self.world.addAgent(self.agent1)
+        self.agent2 = Agent('Agent 2')
+        self.world.addAgent(self.agent2)
+
+        self.agents_dec = []
+        self.agents = [self.agent1, self.agent2]
+        for agent in self.agents:
+            # set agent's params
+            agent.setAttribute('discount', 1)
+            agent.setAttribute('selection', TIEBREAK)
+            # agent.setRecursiveLevel(1)
+
+            # add "decision" variable (0 = didn't decide, 1 = Defected, 2 = Cooperated)
+            dec = self.world.defineState(agent.name, 'decision', list, [NOT_DECIDED, DEFECTED, COOPERATED])
+            self.world.setFeature(dec, NOT_DECIDED)
+            self.agents_dec.append(dec)
+
+        # define agents' actions inspired on TIT-FOR-TAT: first decision is open, then retaliate non-cooperation.
+        # as soon as one agent defects it will always defect from there on
+        for i, agent in enumerate(self.agents):
+            my_dec = self.agents_dec[i]
+            other_dec = self.agents_dec[0 if i == 1 else 1]
+
+            # defect (not legal if other has cooperated before, legal only if agent itself did not defect before)
+            action = agent.addAction({'verb': '', 'action': 'defect'},
+                                     makeTree({'if': equalRow(other_dec, COOPERATED),
+                                               True: {'if': equalRow(my_dec, DEFECTED),
+                                                      True: True,
+                                                      False: False},
+                                               False: True}))
+            tree = makeTree(setToConstantMatrix(my_dec, DEFECTED))
+            self.world.setDynamics(my_dec, action, tree)
+
+            # cooperate (not legal if other or agent itself defected before)
+            action = agent.addAction({'verb': '', 'action': 'cooperate'},
+                                     makeTree({'if': equalRow(other_dec, DEFECTED),
+                                               True: False,
+                                               False: {'if': equalRow(my_dec, DEFECTED),
+                                                       True: False,
+                                                       False: True}}))
+            tree = makeTree(setToConstantMatrix(my_dec, COOPERATED))
+            self.world.setDynamics(my_dec, action, tree)
+
+        # defines payoff matrices (equal to both agents)
+        self.agent1.setReward(get_reward_tree(self.agent1, self.agents_dec[0], self.agents_dec[1]), 1)
+        self.agent2.setReward(get_reward_tree(self.agent2, self.agents_dec[1], self.agents_dec[0]), 1)
+
+        # define order
+        my_turn_order = [{self.agent1.name, self.agent2.name}]
+        self.world.setOrder(my_turn_order)
+
+        # add true mental model of the other to each agent
+        self.world.setMentalModel(self.agent1.name, self.agent2.name, Distribution({self.agent2.get_true_model(): 1}))
+        self.world.setMentalModel(self.agent2.name, self.agent1.name, Distribution({self.agent1.get_true_model(): 1}))
+
+    def run_step(self):
+
+        result = {'Agent 1': {}, 'Agent 2': {}}
+        # decision per step (1 per agent): cooperate or defect?
+        step = self.world.step(debug=result)
+        for i in range(len(self.agents)):
+            logging.info(f'{self.agents[i].name}: {self.world.getFeature(self.agents_dec[i], unique=True)}')
+
+        return_result = {"WORLD": self.world,
+                         "AGENT_DEBUG": result,
+                         "AGENTS": self.agents}
+        return return_result
+
+
+if __name__ == "__main__":
+    # sets up log to screen
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG if DEBUG else logging.INFO)
+    sim = ForwardPlanningTom()
+
+    for h in range(MAX_HORIZON + 1):
+        logging.info('====================================')
+        logging.info(f'Horizon {h}')
+
+        # set horizon (also to the true model!) and reset decisions
+        for i in range(len(sim.agents)):
+            sim.agents[i].setHorizon(h)
+            sim.agents[i].setHorizon(h, sim.agents[i].get_true_model())
+            sim.world.setFeature(sim.agents_dec[i], NOT_DECIDED, recurse=True)
+            for step in range(4):
+                logging.info('====================================')
+                logging.info(f'Step {step}')
+                result = sim.run_step()
---
+++ sim_scripts/ImperfectObservationModel.py
@@ -0,0 +1,122 @@
+import logging
+from psychsim.agent import Agent
+from psychsim.helper_functions import multi_set_matrix, multi_reward_matrix
+from psychsim.pwl import makeTree, equalRow, setToFeatureMatrix, setToConstantMatrix, thresholdRow, CONSTANT
+from psychsim.world import World
+
+__author__ = 'Pedro Sequeira'
+__email__ = 'pedrodbs@gmail.com'
+__description__ = 'Example of setting a incorrect belief over another agent\'s feature. Two agents interact with each ' \
+                  'other: a a consumer agent asks for a certain amount of product while a producer agent produces ' \
+                  'that product. The produced amount depends on the producer\'s production capacity and the asked ' \
+                  'amount, i.e., produced=asked*capacity. The consumer asks for product according to the producer\'s ' \
+                  'capacity: if it observes that the capacity is "full", it will order a "normal" amount, otherwise ' \
+                  'it will ask for double the "normal" amount if it observes that the capacity is "half". ' \
+                  'The consumer\'s reward is dictated by the amount of product received: if it receives the normal ' \
+                  'amount it receives a positive reward, if it receives more than the normal amount it will receive a ' \
+                  'penalty to simulate an over-stock cost.' \
+                  'We set a belief to the consumer agent so that it always believes the consumer is producing at half' \
+                  'capacity when in reality it is not. The agent will therefore always ask for more, thinking that ' \
+                  'this way it will receive what it really needs. This simulates hoarding behavior under false beliefs.'
+
+# parameters
+NUM_STEPS = 4
+HORIZON = 2
+
+DEBUG = False
+
+
+def get_fake_model_name(agent):
+    return 'fake {} model'.format(agent.name)
+
+
+class ImperfectObservationModel:
+    def __init__(self):
+        self.sim_steps = 50
+        # sets up log to screen
+
+        # create world and add agents
+        self.world = World()
+        ag_producer = Agent('Producer')
+        self.world.addAgent(ag_producer)
+        self.ag_consumer = Agent('Consumer')
+        self.world.addAgent(self.ag_consumer)
+        self.agents = [ag_producer, self.ag_consumer]
+
+        # agent settings
+        ag_producer.setAttribute('discount', 1)
+        ag_producer.setHorizon(HORIZON)
+        self.ag_consumer.setAttribute('discount', 1)
+        self.ag_consumer.setHorizon(HORIZON)
+
+        # add variables (capacity and asked/received amounts)
+        self.var_half_cap = self.world.defineState(ag_producer.name, 'half capacity', bool)
+        self.world.setFeature(self.var_half_cap, False)
+        self.var_ask_amnt = self.world.defineState(ag_producer.name, 'asked amount', int, lo=0, hi=100)
+        self.world.setFeature(self.var_ask_amnt, 0)
+        self.var_rcv_amnt = self.world.defineState(self.ag_consumer.name, 'received amount', int, lo=0, hi=100)
+        self.world.setFeature(self.var_rcv_amnt, 0)
+
+        # add producer actions
+        # produce capacity: if half capacity then 0.5*asked amount else asked amount)
+        act_prod = ag_producer.addAction({'verb': '', 'action': 'produce'})
+        tree = makeTree({'if': equalRow(self.var_half_cap, True),
+                         True: multi_set_matrix(self.var_rcv_amnt, {self.var_ask_amnt: 0.5}),
+                         False: setToFeatureMatrix(self.var_rcv_amnt, self.var_ask_amnt)})
+        self.world.setDynamics(self.var_rcv_amnt, act_prod, tree)
+
+        # add consumer actions (ask more = 10 / less = 5)
+        act_ask_more = self.ag_consumer.addAction({'verb': '', 'action': 'ask_more'})
+        tree = makeTree(setToConstantMatrix(self.var_ask_amnt, 10))
+        self.world.setDynamics(self.var_ask_amnt, act_ask_more, tree)
+
+        act_ask_less = self.ag_consumer.addAction({'verb': '', 'action': 'ask_less'})
+        tree = makeTree(setToConstantMatrix(self.var_ask_amnt, 5))
+        self.world.setDynamics(self.var_ask_amnt, act_ask_less, tree)
+
+        # defines payoff for consumer agent: if received amount > 5 then 10 - rcv_amnt (penalty) else rcv_amount (reward)
+        # this simulates over-stock cost, best is to receive max of 5, more than this has costs
+        self.ag_consumer.setReward(
+            makeTree({'if': thresholdRow(self.var_rcv_amnt, 5),
+                      True: multi_reward_matrix(self.ag_consumer, {CONSTANT: 10, self.var_rcv_amnt: -1}),
+                      False: multi_reward_matrix(self.ag_consumer, {self.var_rcv_amnt: 1})}),
+            1)
+
+        # define order (parallel execution)
+        self.world.setOrder([{ag_producer.name, self.ag_consumer.name}])
+
+        # sets consumer belief that producer is at half-capacity, making it believe that asking more has more advantage
+        # - in reality, producer is always at full capacity, so best strategy would be to always ask less
+        self.ag_consumer.setBelief(self.var_half_cap, True)
+        self.total_rwd = 0
+
+    def run_step(self):
+
+        result = {'Consumer': {}, 'Producer': {}}
+        step = self.world.step(debug=result)
+        reward = self.ag_consumer.reward()
+        logging.info('Half capacity:\t\t{}'.format(self.world.getValue(self.var_half_cap)))
+        logging.info('Asked amount:\t\t{}'.format(self.world.getValue(self.var_ask_amnt)))
+        logging.info('Received amount:\t{}'.format(self.world.getValue(self.var_rcv_amnt)))
+        logging.info('Consumer reward:\t{}'.format(reward))
+        self.total_rwd += reward
+
+        logging.info('________________________________')
+        # world.explain(step, level=2)# todo step does not provide outcomes anymore
+
+        return_result = {"WORLD": self.world,
+                         "AGENT_DEBUG": result,
+                         "AGENTS": self.agents}
+        return return_result
+
+
+if __name__ == "__main__":
+    # sets up log to screen
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG if DEBUG else logging.INFO)
+    sim = ImperfectObservationModel()
+    for step in range(10):
+        logging.info('====================================')
+        logging.info(f'Step {step}')
+        result = sim.run_step()
+    logging.info('====================================')
+    logging.info('Total reward: {0}'.format(sim.total_rwd))
