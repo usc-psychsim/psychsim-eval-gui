badfd89d93245cf2f2c19ba0713005b6e736458e
chrisjturner <chrisjturner@gmail.com>
2021-08-30 13:21:56 +0100

    Update appraisal_example_sim.py

--- appraisal/appraisal_example_sim.py
+++ appraisal/appraisal_example_sim.py
@@ -1,14 +1,16 @@
-import sys
-import os
+"""
+Example calculating appraisal dimensions from a sim in the sim_scripts folder.
+This example runs the ImperfectMentalModel3 script, calculates the appraisals from the results, and pickles a 'query'
+for use with the GUI.
 
+NOTE: the psychsim path must be set in order to function
+"""
+import sys
 print(sys.path)
 sys.path.insert(1, "..")
-# sys.path.insert(1, "../../atomic")  # Change this to the relevant paths
 # sys.path.insert(1, "../../psychsim")  # Change this to the relevant paths
 
 import logging
-import pandas as pd
-from appraisal import appraisal_dimensions as ad
 from functions import ASISTQueryFunctions as qf
 import psychsim_gui_helpers as pgh
 from sim_scripts.ImperfectMentalModel3 import ImperfectMentalModel3
@@ -16,12 +18,12 @@ from sim_scripts.ImperfectMentalModel3 import ImperfectMentalModel3
 DEBUG = False
 
 
-def get_appraisal_dimensions(data=None, agent=None, *args, **kwargs):
+def get_appraisal_dimensions(data=None, agent=None):
     """
     Get the appraisal dimensions
     """
     query_functions = qf.ASISTQueryFunctions()
-    player_appraisals = query_functions.get_appraisal_dimensions(data, agent, agent)
+    player_appraisals = query_functions.get_appraisal_dimensions(data, agent, agent, normalise=True)
 
     return player_appraisals
 
@@ -36,10 +38,18 @@ if __name__ == "__main__":
         logging.info(f'Step {step}')
         test_data[step] = sim.run_step()
 
-    run_data = pgh.PsychSimRun(id=None,
+    run_data = pgh.PsychSimRun(id="test",
                                data=test_data,
                                sim_file=__file__,
                                steps=None)
     run_data.data = test_data
     appraisals = get_appraisal_dimensions(data=run_data, agent=sim.ag_consumer.name)
-    pass
+
+    appraisal_results = pgh.PsySimQuery(
+        id="test",
+        params=[],
+        function="test",
+        results=appraisals[1],
+        result_type=appraisals[0]
+    )
+    pgh.save_query_pickle(appraisal_results, "appraisal_test")
